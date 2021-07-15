5476ef83a3a209d3f523f7bc36cb0d7bed7aba12
ksascomm <ksascomm@gmail.com>
2021-07-15 12:19:04 -0400

    breadcrumbs and entry-header padding

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<main id="site-content" class="site-main prose sm:prose lg:prose-lg mx-auto">
-	<div class="breadcrumbs py-4" typeof="BreadcrumbList" vocab="https://schema.org/">
+	<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
 	<?php
 	if ( function_exists( 'bcn_display' ) ) {
 		bcn_display();
