2a36b432eb400de04e99f5635239cad1c83eae2a
ksascomm <ksasweb@jhu.edu>
2021-11-15 09:27:19 -0500

    Add Google Tag Manager & Siteimprove tracking scripts

--- header.php
+++ header.php
@@ -38,6 +38,19 @@
 		<?php endif; ?>
 	</script>
 	<!-- End Google Analytics -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5VTN64C');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+	if ( get_field( 'siteimprove', 'option' ) ): ?>
+	<!-- Siteimprove Analytics -->
+	<script async src="https://siteimproveanalytics.com/js/siteanalyze_11464.js"></script>
+	<!-- End Siteimprove Analytics -->
+	<?php endif;?>
 </head>
 
 <body <?php body_class(); ?>>
