7a3ec4846d3baa58d50c7368025055de7942d1c0
ksascomm <ksasweb@jhu.edu>
2021-12-08 16:29:12 -0500

    Make text & ARIA Label similar

--- template-parts/blocks/slider/slider.php
+++ template-parts/blocks/slider/slider.php
@@ -37,7 +37,7 @@ if ( $is_preview ) {
 				?>
 				<div class="swiper-slide grid grid-cols-1 lg:grid-cols-3 gap-4">
 					<div class="slide-image col-span-2">
-						<?php echo wp_get_attachment_image( $image['id'], 'full', "", array( 'class' => 'swiper-lazy' ) ); ?>
+						<?php echo wp_get_attachment_image( $image['id'], 'full', '', array( 'class' => 'swiper-lazy' ) ); ?>
 						<div class="swiper-lazy-preloader"></div>
 					</div>
 					<div class="slide-caption">
@@ -46,7 +46,7 @@ if ( $is_preview ) {
 						<?php
 						if ( get_sub_field( 'link' ) ) :
 							?>
-							<a class="button p-4 bg-blue hover:bg-blue-light hover:text-primary transform" href="<?php the_sub_field( 'link' ); ?>" aria-label="<?php the_sub_field( 'title' ); ?>">Find Out More <span class="far fa-arrow-alt-circle-right"></span></a>
+							<a class="button p-4 bg-blue hover:bg-blue-light hover:text-primary transform" href="<?php the_sub_field( 'link' ); ?>" aria-label="Find Out More <?php the_sub_field( 'title' ); ?>">Find Out More <span class="sr-only"><?php the_sub_field( 'title' ); ?></span><span class="far fa-arrow-alt-circle-right"></span></a>
 						<?php endif; ?>
 					</div>
 				</div>
