a5e54f8e6909b7828fec1cae9599e2196c4cd822
ksascomm <ksasweb@jhu.edu>
2022-09-01 10:21:06 -0400

    add <meta name="description"

--- inc/open-graph-icons.php
+++ inc/open-graph-icons.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Add Open Graph Meta Info from the actual article data, or customize as necessary.
+ * Add Description Meta Tag and Open Graph Meta Info from the actual article data, or customize as necessary.
  *
  * @package KSAS_Blocks
  */
-function facebook_open_graph() {
+function meta_open_graph() {
 	global $post;
-	if ( ! is_singular() ) { // if it is not a post or a page
+	if ( ! is_singular() ) { // if it is not a post or a page...
 		return;
 	}
 	if ( $excerpt = $post->post_content ) {
-			$excerpt = strip_tags( $post->post_content );
+			$excerpt = wp_strip_all_tags( $post->post_content );
 			$excerpt = str_replace( '', "'", $excerpt );
 			$excerpt = wp_trim_words( $post->post_content, 55, '...' );
 	} else {
 		$excerpt = get_bloginfo( 'title' );
 	}
-
+		echo '<meta name="description" content="' . esc_html( $excerpt ) . '"/>';
 		echo '<meta property="og:title" content="' . esc_html( get_the_title() ) . ' | ' . esc_html( get_bloginfo( 'title' ) ) . '"/>';
 		echo '<meta property="og:description" content="' . esc_html( $excerpt ) . '"/>';
 		echo '<meta property="og:type" content="article"/>';
@@ -24,10 +24,10 @@ function facebook_open_graph() {
 		echo '<meta name="twitter:card" content="summary" />';
 		echo '<meta name="twitter:site" content="@JHUArtsSciences" />';
 
-		// Customize the below with the name of your site
+		// Customize the below with the name of your site.
 		echo '<meta property="og:site_name" content="' . get_bloginfo( 'title' ) . '"/>';
-	if ( ! has_post_thumbnail( $post->ID ) ) { // the post does not have featured image, use a default image
-		// Create a default image on your server or an image in your media library, and insert it's URL here
+	if ( ! has_post_thumbnail( $post->ID ) ) { // the post does not have featured image, use a default image.
+		// Create a default image on your server or an image in your media library, and insert it's URL here.
 		$default_image = get_template_directory_uri() . '/dist/images/favicons/android-icon-192x192.png';
 		echo '<meta property="og:image" content="' . $default_image . '"/>';
 	} else {
@@ -38,7 +38,7 @@ function facebook_open_graph() {
 		echo '
 	';
 }
-add_action( 'wp_head', 'facebook_open_graph', 5 );
+add_action( 'wp_head', 'meta_open_graph', 5 );
 
 
 
