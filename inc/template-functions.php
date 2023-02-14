bf1a7423e31d8d32f57627ef61bf2d5fb2029767
ksascomm <ksasweb@jhu.edu>
2023-02-14 10:59:11 -0500

    Change $title to the_title_attribute()

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -70,10 +70,10 @@ function custom_ksasacademic_page_title( $title ) {
 			return $title;
 		endif;
 	} elseif ( is_single() ) {
-		$title = get_the_title() . ' | ' . get_bloginfo( 'name' ) . ' | Johns Hopkins University';
+		$title = the_title_attribute() . ' | ' . get_bloginfo( 'name' ) . ' | Johns Hopkins University';
 		return $title;
 	} elseif ( is_page() ) {
-		$title = get_the_title() . ' | ' . get_bloginfo( 'name' ) . ' | Johns Hopkins University';
+		$title = the_title_attribute() . ' | ' . get_bloginfo( 'name' ) . ' | Johns Hopkins University';
 		return $title;
 	} elseif ( is_404() ) {
 		$title = 'Page Not Found | ' . get_bloginfo( 'name' ) . ' | Johns Hopkins University';
