381389dcd4f23e0671c345231fa568359383e635
ksascomm <ksasweb@jhu.edu>
2023-05-30 11:59:28 -0400

    Search form CSS fixes

--- searchform.php
+++ searchform.php
@@ -18,5 +18,5 @@ $twentytwenty_aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr
 		<span class="sr-only"><?php _e( 'Search for:', 'ksas-blocks' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></span>
 		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search this site &hellip;', 'placeholder', 'ksas-blocks' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'ksas-blocks' ); ?>" />
+	<input type="submit" class="search-submit bg-blue text-white border-2 p-2 hover:bg-blue-light hover:text-primary" value="<?php echo esc_attr_x( 'Search', 'submit button', 'ksas-blocks' ); ?>" />
 </form>
